d7219f7c643da99a07f0273cf041342f93599e3d
pierresquarra <pierre@squarra.de>
2023-09-12 13:23:45 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import gzip
+import json
+import os
+import openai
+from dotenv import load_dotenv
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+def extract_topics(review):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a topic labeling assistant. Your task is to identify and categorize the topics "
+                           "discussed in the following review into two categories: positive topics and negative "
+                           "topics. Topics should be represented with a single word; if a topic can't be expressed "
+                           "with a single word, please omit it! List the topics in the following structured format: "
+                           "first, list all the positive topics separated by commas (if any), then a semicolon (;), "
+                           "followed by all the negative topics separated by commas (if any). There should always be "
+                           "a semicolon in your response to separate the two categories. \n\nFor example:\nservice,"
+                           "ambiance;pricey,portions\n\n"
+            },
+            {
+                "role": "user",
+                "content": review
+            }
+        ]
+    )
+    return response
+
+
+def parse(path):
+    g = gzip.open(path, 'r')
+    for l in g:
+        yield json.loads(l)
+
+
+def print_reviews(data):
+    for review in data:
+        if "reviewText" in review:
+            print(review['reviewText'])
+
+
+if __name__ == '__main__':
+    path = 'data/AMAZON_FASHION_5.json.gz'
+    data = parse(path)
+    first_entry = next(data)['reviewText']
+    response = extract_topics(first_entry)['choices'][0]['message']['content']
+
+    positive_topics = response.split(';')[0].split(',')
+    negative_topics = response.split(';')[1].split(',')
+
+    print("Review: ", first_entry)
+    print("Positive topics: ", positive_topics)
+    print("Negative topics: ", negative_topics)
