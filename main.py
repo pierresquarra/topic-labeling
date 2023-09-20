e39c61fc16a7620924ef1ea7a988294b78ce5b51
pierresquarra <pierre@squarra.de>
2023-09-20 15:28:35 +0200

    bad

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import gzip
 import json
 import os
 import openai
+import random
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -15,18 +16,24 @@ def extract_topics(review):
             {
                 "role": "system",
                 "content": "You are a topic labeling assistant. Your task is to identify and categorize the topics "
-                "discussed in the following review into two categories: positive topics and negative "
-                "topics. Topics should be represented with a single word; if a topic can't be expressed "
-                "with a single word, please omit it! List the topics in the following structured format: "
-                "first, list all the positive topics separated by commas (if any), then a semicolon (;), "
-                "followed by all the negative topics separated by commas (if any). There should always be "
-                "a semicolon in your response to separate the two categories. \n\nFor example:\nservice,"
-                "ambiance;pricey,portions\n\n",
+                           "discussed in the reviews below into two categories: positive topics and negative topics. "
+                           "Topics should be represented with a single word; if a topic can't be expressed with a "
+                           "single word, please omit it! List the topics in the following structured format: first, "
+                           "list all the positive topics separated by commas (if any), then a semicolon (;), "
+                           "followed by all the negative topics separated by commas (if any). There should always be "
+                           "a semicolon in your response to separate the two categories. For multiple reviews, "
+                           "please separate the labels for each review with a \n delimiter."
             },
             {"role": "user", "content": "Great product and price!"},
             {"role": "assistant", "content": "product,price;\n\n"},
             {"role": "user", "content": "Bad customer support!"},
             {"role": "assistant", "content": ";support\n\n"},
+            {"role": "user",
+             "content": "The staff was very friendly and the food was out of this world. The ambiance of the restaurant was warm and welcoming. However, the prices were a bit steep and the wait time for the food was longer than expected."},
+            {"role": "assistant", "content": "staff,food,ambience;prices,wait\n\n"},
+            {"role": "user",
+             "content": "Review 1: Great product and price\nReview 2: Bad customer support\nReview 3: The staff was very friendly and the food was out of this world. The ambiance of the restaurant was warm and welcoming. However, the prices were a bit steep and the wait time for the food was longer than expected."},
+            {"role": "assistant", "content": "product,price;\n;support\nstaff,food,ambience;prices,wait\n\n"},
             {"role": "user", "content": review},
         ],
     )
@@ -49,12 +56,22 @@ def print_reviews(data):
 if __name__ == "__main__":
     path = "data/AMAZON_FASHION_5.json.gz"
     data = parse(path)
-    first_entry = next(data)["reviewText"]
-    response = extract_topics(first_entry)["choices"][0]["message"]["content"]
+    batch_size = 5  # number of reviews to query from dataset
+
+    all_reviews = list(data)
+    reviews = random.sample(all_reviews, batch_size)
+    reviews_str = "\n".join([f"Review {i + 1}: {review['reviewText']}" for i, review in enumerate(reviews)])
+
+    response = extract_topics(reviews_str)["choices"][0]["message"]["content"]
+    responses = response.split("\n")
 
     positive_topics = response.split(";")[0].split(",")
     negative_topics = response.split(";")[1].split(",")
 
-    print("Review: ", first_entry)
-    print("Positive topics: ", positive_topics)
-    print("Negative topics: ", negative_topics)
+    for i, resp in enumerate(responses):
+        positive_topics = resp.split(";")[0].split(",")
+        negative_topics = resp.split(";")[1].split(",")
+
+        print(f"Review {i + 1}: ", reviews[i]["reviewText"])
+        print("Positive topics: ", positive_topics)
+        print("Negative topics: ", negative_topics)
